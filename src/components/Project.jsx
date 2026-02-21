4fb5bb4cdd4d81ea8f1e6bea35f424b0036b82f8
Tanupriya91 <tp6677@srmist.edu.in>
2026-02-21 23:14:46 +0530

    changes has been updated

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,43 +1,48 @@
-function Project (){
-    return(
-       <section className="project">
-        <h1>My projects</h1>
-        <div className="project1">
-            <div className="left1">
-                <div className="l1-title">
-                    <span>pcpartfinder</span>
-                </div>
-                <div className="l1-content">
-                    <p>A web app that scrapes information from Indian pc component stores and displays them to the user. It can also give price drop alerts to users. (ongoing)</p>
-                </div>
-                <div className="l1-tech-stack">
-                    <span> React, Node.js, PostgreSQL</span> 
-                    
-                     </div>
-                <div className="l1-button">
-                    <div className="website">
-                    <a href="https://pcpartfinder.vercel.app/search" target="_blank" rel="noopener noreferrer">
-              Website
-            </a>
-
-                    </div>
-                    <div className="source code">
-                        <a href="https://github.com/1Gokul/pcpartfinder" target="_blank" rel="noopener noreferrer">
-                        Source Code
-                        </a>
-             
-                    </div>
-                </div>
+function Project() {
+  return (
+    <section className="project">
+      <span className="project-heading">My projects</span>
+      <div className="project1">
+        <div className="left1">
+          <div className="l1-title">
+            <span>pcpartfinder</span>
+          </div>
+          <div className="l1-content">
+            <p>
+              A web app that scrapes information from Indian pc component stores
+              and displays them to the user. It can also give price drop alerts
+              to users. (ongoing)
+            </p>
+          </div>
+          <div className="l1-tech-stack">
+            <span> React, Node.js, PostgreSQL</span>
+          </div>
+          <div className="l1-button">
+            <div className="website">
+              <a
+                href="https://pcpartfinder.vercel.app/search"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Website
+              </a>
             </div>
-            <div className="right1">
-                <img src="imag1.jpg" alt="ss"></img>
+            <div className="source code">
+              <a
+                href="https://github.com/1Gokul/pcpartfinder"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Source Code
+              </a>
             </div>
-
+          </div>
         </div>
-
-        
-
-       </section> 
-    );
+        <div className="right1">
+          <img src="imag1.jpg" alt="ss"></img>
+        </div>
+      </div>
+    </section>
+  );
 }
 export default Project;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,156 @@
 function skills() {
-    return(
-        <section>
+  return (
+    <section className="main-skills">
+      <div className="skills">
+        <div className="skills-title">
+          <span>My toolbox</span>
+        </div>
+        <div className="skills-content">
+          <div className="sub-skillscontent">
+            <div className="sub-skillscontent-title">
+              <span>Frontend</span>
+            </div>
+            <div className="Skillsdata">
+              <div className="data">
+                <div className="data1img">
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
+                    alt=""
+                  ></img>
+                </div>
+                <div className="data1title">
+                  <span>TypeScript</span>
+                </div>
+              </div>
 
-        </section>
-    );
+              <div className="data">
+                <div className="data2img">
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+                    alt=""
+                  ></img>
+                </div>
+                <div className="data2title">
+                  <span>React</span>
+                </div>
+              </div>
+
+              <div className="data">
+                <div className="data3img">
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg"
+                    alt=""
+                  ></img>
+                </div>
+                <div className="data3title">
+                  <span>Next.js</span>
+                </div>
+              </div>
+
+              <div className="data">
+                <div className="data4img">
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gatsby/gatsby-original.svg"
+                    alt=""
+                  ></img>
+                </div>
+                <div className="data4title">
+                  <span>Gatsby</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="sub-skillscontent">
+            <div className="sub-skillscontent-title">
+              <span>Backend</span>
+            </div>
+            <div className="Skillsdata">
+              <div className="data">
+                <div className="data1img">
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
+                    alt=""
+                  ></img>
+                </div>
+                <div className="data1title">
+                  <span>Python</span>
+                </div>
+              </div>
+
+              <div className="data">
+                <div className="data2img">
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+                    alt=""
+                  ></img>
+                </div>
+                <div className="data2title">
+                  <span>Flask</span>
+                </div>
+              </div>
+
+              <div className="data">
+                <div className="data3img">
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg"
+                    alt=""
+                  ></img>
+                </div>
+                <div className="data3title">
+                  <span>FastAPI</span>
+                </div>
+              </div>
+
+              <div className="data">
+                <div className="data4img">
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gatsby/gatsby-original.svg"
+                    alt=""
+                  ></img>
+                </div>
+                <div className="data4title">
+                  <span>MongoDB</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="sub-skillscontent">
+            <div className="sub-skillscontent-title">
+              <span>Others</span>
+            </div>
+            <div className="Skillsdata">
+              <div className="data">
+                <div className="data3img">
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg"
+                    alt=""
+                  ></img>
+                </div>
+                <div className="data3title">
+                  <span>Git</span>
+                </div>
+              </div>
+
+              <div className="data">
+                <div className="data4img">
+                  <img
+                    src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gatsby/gatsby-original.svg"
+                    alt=""
+                  ></img>
+                </div>
+                <div className="data4title">
+                  <span>Unreal Engine 4</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* always use flex flex-direc=column */}
+        </div>
+      </div>
+    </section>
+  );
 }
 export default skills;
